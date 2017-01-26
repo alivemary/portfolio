41d12e921642fc5baa34814f98faa285c92a16bd
alivemary <alivemary@gmail.com>
2017-01-26 11:04:40 +0100

    0.3

--- js/portfolio.js
+++ js/portfolio.js
@@ -14,6 +14,7 @@
             case 'last':
                 $('#aboutMe').removeClass('hidden');
                 $('#projects').removeClass('hidden');
+                break;
         }
     });
 });

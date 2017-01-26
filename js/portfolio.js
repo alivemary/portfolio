bcf46d974123d2a608ad698badb74eb69e39a5e3
alivemary <alivemary@gmail.com>
2017-01-26 11:02:18 +0100

    0.2

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,1 +1,19 @@
-﻿
+﻿$(document).ready(function () {
+   
+    $('.nav-item').on('click', function () {
+        console.log(this.id);
+        switch (this.id) {
+            case 'aM':
+                $('#aboutMe').removeClass('hidden');
+                $('#projects').addClass('hidden');
+                break;
+            case 'pR':
+                $('#aboutMe').addClass('hidden');
+                $('#projects').removeClass('hidden');
+                break;
+            case 'last':
+                $('#aboutMe').removeClass('hidden');
+                $('#projects').removeClass('hidden');
+        }
+    });
+});
